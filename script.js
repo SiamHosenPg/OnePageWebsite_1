b257825da768e20af9f9ccc9af85fafa6ba53464
Siam Hosen <siamhosenpg@gmail.com>
2024-10-15 11:19:54 +0600

    Menu Bar show hidden System

---
+++ script.js
@@ -0,0 +1,10 @@
+
+   
+ const MenuMbBox = document.querySelector(".Header .NavMbScreen");
+        function MenuClickHeader() {
+            MenuMbBox.classList.add('Active');
+        }
+        function CLossMenuBrMbScreen() {
+            MenuMbBox.classList.remove('Active');
+        }
+        // Mehu Mar Show Hiddne system  #####
